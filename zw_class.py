e0c8b5d993bb5a47fa47efe2ee28b411c066a407
Christoph Huckle <chrihuc@gmail.com>
2017-01-26 19:07:20 +0100

    zwave

--- zw_class.py
+++ zw_class.py
@@ -11,98 +11,113 @@ import openzwave
 from openzwave.option import ZWaveOption
 from openzwave.network import ZWaveNetwork
 from louie import dispatcher
+import zw_config
 
 device="/dev/ttyACM0"
 log="None"
-sniff=300.0
 
-#Define some manager options
-options = ZWaveOption(device, \
-  config_path="/home/christoph/spyder/python-openzwave/openzwave/config", \
-  user_path=".", cmd_line="")
-options.set_log_file("OZW_Log.log")
-options.set_append_log_file(False)
-options.set_console_output(False)
-options.set_save_log_level(log)
-options.set_logging(True)
-options.lock()
+class zwave(object):
 
-def louie_network_started(network):
-    print("Hello from network : I'm started : homeid {:08x} - {} nodes were found.".format(network.home_id, network.nodes_count))
+    #Define some manager options
+    options = ZWaveOption(device, \
+      config_path="/home/christoph/spyder/python-openzwave/openzwave/config", \
+      user_path=".", cmd_line="")
+    options.set_log_file("OZW_Log.log")
+    options.set_append_log_file(False)
+    options.set_console_output(False)
+    options.set_save_log_level(log)
+    options.set_logging(True)
+    options.lock()
+    
+    def louie_network_started(self, network):
+        print("Hello from network : I'm started : homeid {:08x} - {} nodes were found.".format(network.home_id, network.nodes_count))
+    
+    def louie_network_failed(self, network):
+        print("Hello from network : can't load :(.")
+    
+    def louie_network_ready(self, network):
+        print("Hello from network : I'm ready : {} nodes were found.".format(network.nodes_count))
+        print("Hello from network : my controller is : {}".format(network.controller))
+        dispatcher.connect(self.louie_node_update, ZWaveNetwork.SIGNAL_NODE)
+        dispatcher.connect(self.louie_value_update, ZWaveNetwork.SIGNAL_VALUE)
+    
+    def louie_node_update(self, network, node):
+        print("Hello from node : {}.".format(node))
+    
+    def louie_value_update(self, network, node, value):
+        print("Hello from value : {}.".format( value ))
+    #    home_id: [0xeefad666] id: [72057594093060096] parent_id: [3] label: [Switch] data: [False].
+    #    home_id: [0xeefad666] id: [72057594093273218] parent_id: [3] label: [Power] data: [0.0].
+    #    home_id: [0xeefad666] id: [144115188131201026] parent_id: [3] label: [Energy] data: [0.00999999977648]
 
-def louie_network_failed(network):
-    print("Hello from network : can't load :(.")
+    def __init__(self):
+        #Create a network object
+        self.network = ZWaveNetwork(self.options, autostart=False)
+        
+        
+        #We connect to the louie dispatcher
+        dispatcher.connect(self.louie_network_started, ZWaveNetwork.SIGNAL_NETWORK_STARTED)
+        dispatcher.connect(self.louie_network_failed, ZWaveNetwork.SIGNAL_NETWORK_FAILED)
+        dispatcher.connect(self.louie_network_ready, ZWaveNetwork.SIGNAL_NETWORK_READY)
+        
+        self.network.start()
 
-def louie_network_ready(network):
-    print("Hello from network : I'm ready : {} nodes were found.".format(network.nodes_count))
-    print("Hello from network : my controller is : {}".format(network.controller))
-    dispatcher.connect(louie_node_update, ZWaveNetwork.SIGNAL_NODE)
-    dispatcher.connect(louie_value_update, ZWaveNetwork.SIGNAL_VALUE)
+        #We wait for the network.
+        print("***** Waiting for network to become ready : ")
+        for i in range(0,90):
+            if self.network.state>=self.network.STATE_READY:
+                print("***** Network is ready")
+                break
+            else:
+                sys.stdout.write(".")
+                sys.stdout.flush()
+                time.sleep(1.0)
 
-def louie_node_update(network, node):
-    print("Hello from node : {}.".format(node))
+            #time.sleep(5.0)
+            
+            #We update the name of the controller
+            #print("Update controller name")
+            #network.controller.node.name = "Hello name"
+            
+            #time.sleep(5.0)
+            
+            #We update the location of the controller
+            #print("Update controller location")
+            #network.controller.node.location = "Hello location"
+            
+            
+            for node in self.network.nodes:
+                for val in self.network.nodes[node].get_switches() :
+                    print("Switch : {}".format(self.network.nodes[node]))
+                    print("Switch1: {}".format(val))
+            #        72057594093060096
+            #       144115188130988032
+            #        network.nodes[node].set_switch(val,True)
+                #We only activate the first switch
+                #exit
+            
+            
+            for node in self.network.nodes:
+                for val in self.network.nodes[node].get_dimmers() :
+                   print("Dimmer : {}".format(self.network.nodes[node]))
+                   print("Switch1: {}".format(val))
+            #        72057594093076481
+            #        144115188131004513
+            #        144115188131004417
+            #        72057594093076577 
+            #        network.nodes[node].set_dimmer(val,80)
+                #We only activate the first dimmer
+                #exit
 
-def louie_value_update(network, node, value):
-    print("Hello from value : {}.".format( value ))
+    def end_network(self):
+        self.network.stop()
+        
+    def set_device(self, data_ev): 
+#       TODO do threaded with stop criteria
+        if zw_config.devices[data_ev.get('Device')] == 'IO16o':
+            return self.set_io16(data_ev.get('Device'),data_ev.get('Value'))        
 
-#Create a network object
-network = ZWaveNetwork(options, autostart=False)
-
-
-#We connect to the louie dispatcher
-dispatcher.connect(louie_network_started, ZWaveNetwork.SIGNAL_NETWORK_STARTED)
-dispatcher.connect(louie_network_failed, ZWaveNetwork.SIGNAL_NETWORK_FAILED)
-dispatcher.connect(louie_network_ready, ZWaveNetwork.SIGNAL_NETWORK_READY)
-
-network.start()
-
-#We wait for the network.
-print("***** Waiting for network to become ready : ")
-for i in range(0,90):
-    if network.state>=network.STATE_READY:
-        print("***** Network is ready")
-        break
-    else:
-        sys.stdout.write(".")
-        sys.stdout.flush()
-        time.sleep(1.0)
-
-#time.sleep(5.0)
-
-#We update the name of the controller
-#print("Update controller name")
-#network.controller.node.name = "Hello name"
-
-#time.sleep(5.0)
-
-#We update the location of the controller
-#print("Update controller location")
-#network.controller.node.location = "Hello location"
-
-
-for node in network.nodes:
-    for val in network.nodes[node].get_switches() :
-        print("Switch : {}".format(network.nodes[node]))
-        print("Switch1: {}".format(val))
-#        72057594093060096
-#       144115188130988032
-#        network.nodes[node].set_switch(val,True)
-    #We only activate the first switch
-    #exit
-
-
-for node in network.nodes:
-    for val in network.nodes[node].get_dimmers() :
-       print("Dimmer : {}".format(network.nodes[node]))
-       print("Switch1: {}".format(val))
-#        72057594093076481
-#        144115188131004513
-#        144115188131004417
-#        72057594093076577
-#        network.nodes[node].set_dimmer(val,80)
-    #We only activate the first dimmer
-    #exit
-
-raw_input('Press key to exit\n') 
-
-network.stop()
+if __name__ == "__main__":
+    zwnw = zwave()
+    raw_input('Press key to exit\n') 
+    zwnw.end_network()
---
+++ zw_config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 26 07:19:15 2017
+
+@author: christoph
+"""
+
+# translation from homecontrol addresses to tifo addresses
+devices = {'72057594093060096':'V00KUE1DEK1LI01','144115188130988032':'V00KUE1DEK1LI02',
+          '6De9SU.sFC':'V00WOH1RUM1MD01',
+          '63mHZj.voh':'V00WOH1RUM1SI01','63mHZj.woZ':'V00WOH1RUM1TE11'}
