080405c2155544d9dbde54587f59dd2c4a9d8340
Christoph Huckle <chrihuc@gmail.com>
2018-03-25 15:18:51 +0200

    epaper partly done

--- hat_display.py
+++ hat_display.py
@@ -10,60 +10,37 @@ import time
 import spidev as SPI
 import EPD_driver
 import datetime
+import udp_send
 
 EPD2X9 = 0
 EPD02X13 = 1
 EPD1X54 = 0
 
-bus = 0 
+bus = 0
 device = 0
-DELAYTIME = 1.5	
+DELAYTIME = 1.5
 
 disp = EPD_driver.EPD_driver(spi=SPI.SpiDev(bus, device))
 
-#init and Clear full screen
-print '------------init and Clear full screen------------'
+
 disp.Dis_Clear_full()
-
-#Show full pic
-print '------------Show full pic------------'
-#disp.Dis_full_pic(waveshare)
-time.sleep(DELAYTIME)
-
-#init and Clear part screen
-print '------------init and Clear part screen------------'
 disp.Dis_Clear_part()
 
-#Show part pic
-print '------------Show part pic------------'
-#disp.Dis_part_pic(0,xDot-1,0,yDot-1,waveshare)
-time.sleep(DELAYTIME)
-
-#Drawing
-print '------------Show Circle and Line------------'
-#disp.Dis_Drawing((yDot-32)/8,5,Circle3232,32,32) #Circle
-#disp.Dis_Drawing((yDot-32)/8,0,Line3232,32,32) #Line
-time.sleep(DELAYTIME)
-
-#String
-print '------------Show string------------'
-disp.Dis_String(0, 10, "SHOW TIME : ",16)
-disp.Dis_String(0, 26, "I am an electronic paper display",12)
-time.sleep(DELAYTIME)
-
-#Progress
-print '------------Show Progress------------'
-disp.Dis_Progress(9)	
-time.sleep(DELAYTIME)
+#disp.Dis_String(0, 10, "SHOW TIME : ",16)
+#disp.Dis_String(0, 26, "I am an electronic paper display",12)
 
 #time
 print '------------show time------------'
 while 1 :
-	now = datetime.datetime.now()
-	now_sec = now.second%10
-	next_sec = 11  #Guaranteed next greater than 9
-	if now_sec != next_sec:
-		disp.Dis_showtime(now.hour,now.minute,now.second)
-		next_sec = now.second%10
-		
-		
+    now = datetime.datetime.now()
+#	now_sec = now.second%10
+#	next_sec = 11  #Guaranteed next greater than 9
+#	if now_sec != next_sec:
+#		disp.Dis_showtime(now.hour,now.minute,now.second)
+#		next_sec = now.second%10
+    inp_dict = udp_send.bidirekt('Inputs')
+    set_dict = udp_send.bidirekt('Settings')
+    disp.Dis_showtime(now.hour,now.minute,0)
+    disp.Dis_String(10, 26, inp_dict['A00TER1GEN1TE01'] + " grad Celsius",16)
+    disp.Dis_String(10, 42, set_dict['Status'] + " grad Celsius",16)
+    time.sleep(60)
