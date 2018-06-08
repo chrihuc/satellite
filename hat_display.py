cb9b5fd43b698cf4ca263a7958906da45c1e8b2d
Christoph Huckle <chrihuc@gmail.com>
2018-06-08 20:58:01 +0200

    ...

--- hat_display.py
+++ hat_display.py
@@ -14,29 +14,26 @@ import time
 
 epd = epd2in13.EPD()
 epd.init(epd.lut_full_update)
+fontTime = ImageFont.truetype('./display/FreeMonoBold.ttf', 16)
+fontStatus = ImageFont.truetype('./display/FreeMonoBold.ttf', 18)
+epd.delay_ms(2000)
 
 image = Image.new('1', (epd2in13.EPD_HEIGHT, epd2in13.EPD_WIDTH), 255)  # 255: clear the frame
 draw = ImageDraw.Draw(image)
 image_width, image_height  = image.size
 draw.rectangle((0, 0, image_width, image_height), fill = 0)
-epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 0)
 
+epd.clear_frame_memory(0xFF)
+epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 0)
 epd.display_frame()
 
 draw.rectangle((0, 0, image_width, image_height), fill = 255)
-#draw.rectangle((0, 10, 128, 30), fill = 0)
-fontTime = ImageFont.truetype('./display/FreeMonoBold.ttf', 16)
-fontStatus = ImageFont.truetype('./display/FreeMonoBold.ttf', 18)
-
 epd.clear_frame_memory(0xFF)
 epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 0)
 epd.display_frame()
 epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 0)
 epd.display_frame()
-epd.delay_ms(2000)
-epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 0)
 
-epd.display_frame()
         
 def main():
     while 1:
@@ -51,7 +48,7 @@ def main():
         #draw.text((10, 58), 'Aussen: ' + inp_dict['A00TER1GEN1TE01'] + " degC ", font = fontTime, fill = 0)
         draw.text((0, 74), 'Status: ' + set_dict['Status'], font = fontStatus, fill = 0)
         #draw.text((10, 90), 'Status: ' + set_dict['Status'], font = fontTime, fill = 0)
-        epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 80)
+        epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 0)
         epd.display_frame()
         
         time.sleep(60)        
