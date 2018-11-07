8f93c08f227bf085c01a88aaf7aa0b05d3c2bfac
Christoph Huckle <chrihuc@gmail.com>
2018-11-07 05:53:18 +0100

    added modul

---
+++ mqtt_publish.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+@author: christoph
+"""
+
+import constants
+import paho.mqtt.client as mqtt
+import json
+import datetime
+import time
+from time import localtime,strftime
+
+
+zeit =  time.time()
+uhr = str(strftime("%Y-%m-%d %H:%M:%S",localtime(zeit)))
+client = mqtt.Client(constants.name +'_pub_' + uhr)
+client.username_pw_set(username=constants.mqtt_.user,password=constants.mqtt_.password)
+client.connect(constants.mqtt_.server)
+client.loop_start()
+
+def handler(obj):
+    if hasattr(obj, 'isoformat'):
+        return obj.isoformat()
+    elif isinstance(obj, datetime.timedelta):
+        return obj.seconds
+    else:
+        return 'non Json'
+
+def mqtt_pub(channel, data, retain=True):
+    zeit =  time.time()
+    uhr = str(strftime("%Y-%m-%d %H:%M:%S",localtime(zeit)))    
+    if isinstance(data, dict):
+        data['ts'] = uhr
+        data = json.dumps(data, default=handler, allow_nan=False)
+        client.publish(channel, data, qos=1, retain=retain)
+    else:
+        raise TypeError, 'Data is not a dictionary'
