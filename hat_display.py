ae4c22b72db9380b2768efc5a65391fa9e7b524f
Christoph Huckle <chrihuc@gmail.com>
2019-01-28 19:57:34 +0100

    test

--- hat_display.py
+++ hat_display.py
@@ -26,35 +26,22 @@ fontStatus = ImageFont.truetype('./display/FreeMonoBold.ttf', 18)
 image = Image.new('1', (epd2in13.EPD_HEIGHT, epd2in13.EPD_WIDTH), 255)  # 255: clear the frame
 draw = ImageDraw.Draw(image)
 image_width, image_height  = image.size
-draw.rectangle((0, 0, image_width, image_height), fill = 255)
-
-#epd.clear_frame_memory(255)
-#epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 0)
-#epd.display_frame()
-#
 #draw.rectangle((0, 0, image_width, image_height), fill = 255)
-#epd.clear_frame_memory(255)
-#epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 0)
-#epd.display_frame()
-#epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 0)
-#epd.display_frame()
 
-## neu leeren
-#emptimage = Image.new('1', (epd2in13.EPD_WIDTH, 32), 255)
-#emptdraw = ImageDraw.Draw(emptimage)
-#emptimage_width, emptimage_height  = emptimage.size
-#for k in range(0, 3):
-#    emptdraw.rectangle((0, 0, emptimage_width, emptimage_height), fill = 255)
-#    epd.set_frame_memory(emptimage.transpose(Image.ROTATE_270), 0, k * 32)
-#    epd.display_frame()
 
 epd.clear_frame_memory(0xFF)
 epd.display_frame()
 epd.clear_frame_memory(0xFF)
 epd.display_frame()
 
-if True:
-    pix_size = 64 # 32 ging 64 nicht
+# neu nach working
+epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 0)
+epd.display_frame()
+epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 0)
+epd.display_frame()
+
+if False:
+    pix_size = 64 # 32 ging 64 nicht, mit anderem USB Ladegerät ging dann auch 64
     pixel = Image.new('1', (pix_size,pix_size), 0)
     for x in range(0,epd2in13.EPD_WIDTH,pix_size):
         for y in range(0,epd2in13.EPD_HEIGHT,pix_size):
@@ -63,12 +50,9 @@ if True:
                 epd.display_frame()
             print(x,y)
 
-print('Cleared')
-#image.save('./1.png', "PNG")
-
 mqtt.Client.connected_flag=False
 client = None
-topics = ["Settings/Status", "Inputs/A00TER1GEN1TE01", "Inputs/V00WOH1RUM1TE01", "Inputs/V00WOH1RUM1TE02"]
+topics = ["Settings/Status", "Inputs/A00TER1GEN1TE01", "Inputs/V00WOH1RUM1TE01", "Inputs/V00WOH1RUM1TE02", 'Time']
 ipaddress = constants.mqtt_.server
 port = 1883
 
@@ -80,7 +64,6 @@ def connect(ipaddress, port):
     assign_handlers(on_connect, dis_con, on_message)
     client.username_pw_set(username=constants.mqtt_.user,password=constants.mqtt_.password)
     client.connect(ipaddress, port, 60)
-#    client.loop_start()
     client.loop_forever()
 
 def assign_handlers(connect, disconnect, message):
@@ -118,16 +101,27 @@ values = {'Time': '',
           'V00WOH1RUM1TE01': '',
           'Status':''}
 
+draw.text((0, 0), 'Time: ' + values['Time'] + u"    ", font = fontTime, fill = 0)
+draw.text((0, 26), 'Aussen: ' + values['A00TER1GEN1TE01'] + u"    ", font = fontTime, fill = 0)
+draw.text((0, 42), 'Innen: ' + values['V00KUE1RUM1TE02'] +  u"    ", font = fontTime, fill = 0)
+draw.text((100, 42), '/ ' + values['V00WOH1RUM1TE01'] + u"    ", font = fontTime, fill = 0)
+draw.text((0, 74), 'Status: ' + values['Status'], font = fontStatus, fill = 0)
+epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 0)
+epd.display_frame()
+
+# ab hier nur partial update
+epd.init(epd.lut_partial_update)
+
 def on_message(client, userdata, msg):
 #    print(msg.topic + " " + str(msg.payload))
 #    retained = msg.retain
     message = str(msg.payload.decode("utf-8"))
     try:
         m_in=(json.loads(message)) #decode json data
-#        draw = ImageDraw.Draw(image)
+
         draw.rectangle((0, 0, image_width, image_height), fill = 255)
         redraw = False
-#        print(m_in)
+
         if 'Status' in m_in.values():
             values['Status'] = m_in['Value']
             redraw = True
@@ -140,36 +134,18 @@ def on_message(client, userdata, msg):
         elif 'V00WOH1RUM1TE01' in m_in.values():
             values['V00WOH1RUM1TE01'] = m_in['Value']
             redraw = True
-        elif 'Time' in m_in.values():
+        elif 'Time' in msg.topic:
             values['Time'] = m_in['Value']
             redraw = True
         if redraw:
-#            draw.rectangle((0, 0, image_width, image_height), fill = 255)
             draw.text((0, 0), 'Time: ' + values['Time'] + u"    ", font = fontTime, fill = 0)
-#            epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 0)
-#            epd.display_frame()
-
-#            draw.rectangle((0, 0, image_width, image_height), fill = 255)
             draw.text((0, 26), 'Aussen: ' + values['A00TER1GEN1TE01'] + u"    ", font = fontTime, fill = 0)
-#            epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 0)
-#            epd.display_frame()
-#
-#            draw.rectangle((0, 0, image_width, image_height), fill = 255)
             draw.text((0, 42), 'Innen: ' + values['V00KUE1RUM1TE02'] +  u"    ", font = fontTime, fill = 0)
-#            epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 0)
-#            epd.display_frame()
-#
-#            draw.rectangle((0, 0, image_width, image_height), fill = 255)
-            draw.text((80, 42), '/ ' + values['V00WOH1RUM1TE01'] + u"    ", font = fontTime, fill = 0)
-#            epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 0)
-#            epd.display_frame()
-#
-#            draw.rectangle((0, 0, image_width, image_height), fill = 255)
+            draw.text((100, 42), '/ ' + values['V00WOH1RUM1TE01'] + u"    ", font = fontTime, fill = 0)
             draw.text((0, 74), 'Status: ' + values['Status'], font = fontStatus, fill = 0)
             epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 0)
             epd.display_frame()
-#            epd.set_frame_memory(image.transpose(Image.ROTATE_90), 0, 0)
-#            epd.display_frame()
+
             image.save('./1.png', "PNG")
     except Exception as e:
         print('Error on', e)
